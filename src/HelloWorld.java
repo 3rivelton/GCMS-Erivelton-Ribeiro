90fc0c00ab0c0319582de881b8082c822869e022
3rivelton <erivelton.r.alves@gmail.com>
2023-02-10 22:30:12 -0300

    meu terceiro commit

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,8 +3,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 
-		System.out.println("Hello Word!");
-		System.out.println("Ola mundo");
+		new Print();
 
 	}
 
---
+++ src/Print.java
@@ -0,0 +1,12 @@
+
+public class Print {
+	
+	public Print (){
+		
+		System.out.println("Hello Word!");
+		System.out.println("Ola mundo");
+		System.out.println("Ola mundo querido");
+		
+	}
+
+}
